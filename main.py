df6667b34f9fee58d51ebf61cdf3417639d190ed
turmezzz <tzmakoev@edu.hse.ru>
2020-08-18 18:21:38 +0300

    Added detector wrapper, requirements.txt, reworked visualizer

---
+++ main.py
@@ -0,0 +1,61 @@
+"""
+this main.py file is fo testing and debugging
+"""
+
+import PIL.Image
+import numpy as np
+
+from detector import Detector
+from mrcnn import _visualize
+
+
+def convert_for_visualizer(results):
+    amount = len(results)
+
+    if amount == 0:
+        return np.empty(0)
+
+    roiss_list = []
+    masks_list = np.zeros((results[0].mask.shape[0], results[0].mask.shape[1], amount))
+    class_ids_list = []
+    scores_list = []
+    for i in range(amount):
+        roiss_list.append(results[i].rois)
+        masks_list[..., i] = results[i].mask
+        class_ids_list.append(results[i].class_id)
+        scores_list.append(results[i].score)
+
+    return np.array(roiss_list), masks_list, np.array(class_ids_list), np.array(scores_list)
+
+
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+file_path = '/Users/turmezzz/Desktop/ira.png'
+
+raw_image = PIL.Image.open(file_path)
+rgb_image = raw_image.convert('RGB')
+rgb_image = np.asarray(rgb_image)
+
+detector = Detector(0.5)
+box = convert_for_visualizer(detector.detect_image(rgb_image))
+if len(box) != 0:
+    _visualize.display_instances(rgb_image,
+                                 box[0],
+                                 box[1],
+                                 box[2],
+                                 class_names,
+                                 box[3])
